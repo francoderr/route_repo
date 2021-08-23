3625d5b9a6edb8a57901809d928cf1e0e40d3bc4
korra <korrasnipes@gmail.com>
2021-08-23 21:54:43 +0300

    changed app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 
-router.get('/home', (req, res) => {
+router.get('/', (req, res) => {
     res.send('Hello World, This is the home router');
 });
 
